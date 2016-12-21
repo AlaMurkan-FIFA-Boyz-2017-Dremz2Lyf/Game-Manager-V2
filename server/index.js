5bca2dafe0b49efe790e2ee5f45ce4ef2fc6c26f
Scott Horn <scotthorn0@gmail.com>
2016-12-21 17:24:11 -0600

    Creates package.json, skeleton server, and routes

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 4040;
+
+const server = app.listen(port, ()=> {
+  console.log(`Listening on port ${port}`);
+
+})
