2fdb9e89f9eab5df55e2e6977a62d1e2dffab196
Scott Horn <scotthorn0@gmail.com>
2016-12-30 23:12:34 -0800

    Move Seed directory, issue with path to it. 'BeforeEach' hook for server set up.

--- test/server/index_test.js
+++ test/server/index_test.js
@@ -4,14 +4,12 @@ const request = require('supertest-as-promised');
 const routes = require(__server + '/index.js');
 const _ = require(__server + '/utilities.js');
 const sinon = require('sinon');
+const db = require(__server + '/db.js');
 
-
-// NOTE: This will be used after knex is configured and the database is set up
-// const config = require('../../knexfile.js');
 
 describe('The Server', function() {
 
-  // const knex = require('knex')(config[env]);
+
 
   let app = TestHelper.createApp();
 
@@ -68,26 +66,27 @@ describe('The Server', function() {
 
   });
 
+  describe('Base root "/"', () => {
+    it_('should serve the static files on calls to the root', function * () {
+
+      yield request(app)
+      .get('/')
+      .expect(200)
+      .expect((res) => {
+        expect(res.text).to.be.an('string');
+        expect(TestHelper.checkForHtml(res.text)).to.equal('<html>');
+      });
+    });
+  });
+
   describe('Routes', function() {
 
-    // beforeEach(function(done) {
-    //   knex.migrate.rollback().then(res => {
-    //     knex.migrate.latest().then(res => {
-    //       return knex.seed.run().then(res => {
-    //         done();
-    //       });
-    //     });
-    //   });
-    // });
-    describe('root', () => {
-      it_('should serve the static files on calls to the route', function * () {
-
-        yield request(app)
-        .get('/')
-        .expect(200)
-        .expect((res) => {
-          expect(res.text).to.be.an('string');
-          expect(TestHelper.checkForHtml(res.text)).to.equal('<html>');
+    beforeEach(function(done) {
+      db.migrate.rollback().then(res => {
+        db.migrate.latest().then(res => {
+          return db.seed.run().then(res => {
+            done();
+          });
         });
       });
     });
@@ -95,7 +94,6 @@ describe('The Server', function() {
     describe('/games', function() {
 
       describe('GET', () => {
-
         it_('should respond with all the games when no id or type param is passed', function * () {
 
           yield request(app)
@@ -257,38 +255,38 @@ describe('The Server', function() {
   //
   //   });
   //
-  //   describe('/players', function() {
-  //     describe('GET', () => {
-  //
-  //       it_('should respond with a status of 200', function * () {
-  //
-  //         yield request(app)
-  //         .get('/players')
-  //         .expect(200);
-  //       });
-  //     });
-  //
-  //     describe('POST', () => {
-  //
-  //       it_('should respond with a status of 200', function * () {
-  //
-  //         yield request(app)
-  //         .post('/players')
-  //         .expect(201);
-  //       });
-  //     });
-  //
-  //     describe('PUT', () => {
-  //
-  //       it_('should respond with a status of 200', function * () {
-  //
-  //         yield request(app)
-  //         .put('/players')
-  //         .expect(202);
-  //       });
-  //     });
-  //
-  //   });
+    describe('/players', function() {
+      describe('GET', () => {
+
+        it_('should respond with a status of 200', function * () {
+
+          yield request(app)
+          .get('/players')
+          .expect(200);
+        });
+      });
+
+      describe('POST', () => {
+
+        it_('should respond with a status of 201', function * () {
+
+          yield request(app)
+          .post('/players')
+          .expect(201);
+        });
+      });
+
+      describe('PUT', () => {
+
+        it_('should respond with a status of 202', function * () {
+
+          yield request(app)
+          .put('/players')
+          .expect(202);
+        });
+      });
+
+    });
   });
 
 });
