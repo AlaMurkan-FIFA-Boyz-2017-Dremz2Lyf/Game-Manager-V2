75aec38eca76d33cbae9f3675cf238df4d267a53
Scott Horn <scotthorn0@gmail.com>
2017-04-05 18:21:30 -0500

    Working on scripts and knexfile for deployment.

--- knexfile.js
+++ knexfile.js
@@ -22,13 +22,7 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
+      database: 'v2'
     },
     migrations: {
       tableName: 'knex_migrations'
