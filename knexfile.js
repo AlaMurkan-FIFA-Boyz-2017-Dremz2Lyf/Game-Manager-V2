69074087e8528675e1672b7c17c91768255d97b7
Scott Horn <scotthorn0@gmail.com>
2017-04-07 13:51:35 -0500

    testing knexfile

--- knexfile.js
+++ knexfile.js
@@ -21,9 +21,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: process.env.DATABASE_URL
-    },
+    connection: process.env.DATABASE_URL,
     migrations: {
       tableName: 'knex_migrations'
     },
